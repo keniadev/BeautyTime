1769465221fba410b73429205e74cfa08f1f764c
keniadev <kenializbethcontreras@outlook.es>
2025-08-22 10:38:34 -0600

    Controlador de cita y cupo

--- src/main/java/org/esfe/BeautyTimeApp/servicios/interfaces/ICupoService.java
+++ src/main/java/org/esfe/BeautyTimeApp/servicios/interfaces/ICupoService.java
@@ -1,9 +1,12 @@
 package org.esfe.BeautyTimeApp.servicios.interfaces;
 
 import org.esfe.BeautyTimeApp.modelos.Cupo;
+import org.esfe.BeautyTimeApp.modelos.Servicio;
+import org.esfe.BeautyTimeApp.modelos.Turno;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,4 +21,12 @@ public interface ICupoService {
     Cupo crearOEditar(Cupo cupo);
 
     void eliminarPorId(Integer id);
+
+    List<Cupo> obtenerCuposPorServicioYFecha(Servicio servicio, LocalDate fecha);
+
+    Optional<Cupo> buscarCupoPorServicioFechaTurno(Servicio servicio, LocalDate fecha, Turno turno);
+
+    boolean hayCupoDisponible(Servicio servicio, LocalDate fecha, Turno turno);
+
+    Cupo ocuparCupo(Servicio servicio, LocalDate fecha, Turno turno);
 }
